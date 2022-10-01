7d5ac1d4af5ab9825220651071c0e99a1d1689ed
DevSankalp <94849180+DevSankalp@users.noreply.github.com>
2022-10-02 01:58:29 +0530

    modal

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,13 +1,19 @@
 import React from "react"
-import model from "../assets/models/ISS/iss_2010.glb"
-
+import Model1 from "../assets/models/ISS/iss_1998.glb"
+import Model2 from "../assets/models/ISS/iss_2000.glb"
+import Model3 from "../assets/models/ISS/iss_2001.glb"
+import Model4 from "../assets/models/ISS/iss_2002.glb"
+import Model5 from "../assets/models/ISS/iss_2007.glb"
+import Model6 from "../assets/models/ISS/iss_2008.glb"
+import Model7 from "../assets/models/ISS/iss_2010.glb"
+import Model8 from "../assets/models/ISS/iss_2012.glb"
 
 export default function Model() {
     return (
-        <div className="model_div">
+        <div className="model_div col-start-2 w-[100%]">
             <model-viewer
                 camera-orbit="-30deg auto auto" max-camera-orbit="auto 100deg auto" shadow-intensity="1" camera-controls touch-action="pan-y" disable-zoom 
-                src={model}
+                src={Model1}
                 auto-rotate
                 style={{ width: "600px", height: "600px"}}
             >
