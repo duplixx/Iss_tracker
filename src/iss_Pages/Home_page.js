ff21fabe3318a240f81b3d50eb885abaf02c9ae5
DevSankalp <94849180+DevSankalp@users.noreply.github.com>
2022-10-02 09:10:49 +0530

    speedometer position

--- src/iss_Pages/Home_page.js
+++ src/iss_Pages/Home_page.js
@@ -2,10 +2,12 @@ import React from "react"
 import "../home_page.css"
 import Header from "../home_page_components/Header"
 import Hero from "../home_page_components/Hero"
+import Modal from "../components/ModalPage"
 
 export default function Home_page() {
     return (
         <>
+            {/* <Modal/> */}
             <Header/>
             <Hero/> 
         </>
